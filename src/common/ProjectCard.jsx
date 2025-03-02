9dcb98c74ef5d65b9c998b1708aeef26f9c02136
aliabbas34 <aliabbas7317@gmail.com>
2025-03-02 17:57:25 +0530

    project card styling done, actual data need to be added

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,12 +1,33 @@
+import githubLight from '../assets/github-light.svg';
+import githubDark from '../assets/github-dark.svg';
+import { useTheme } from '../common/ThemeContext';
 
-function ProjectCard({ link, src, h3, p }){
+function ProjectCard({ link, src, h3, p, projDescription, projName, icons }){
+
+    const { theme } = useTheme();
+    const githubIcon = theme === 'light' ? githubLight : githubDark ;
+
     return(
-        <a href={link} target='_blank'>
-            <img src={src} className="hover" alt={`${h3} logo`} />
-            <h3>{h3}</h3>
-            <p>{p}</p>
-        </a>
+        <div id="projCard">
+            <h3 id="projTitle">{projName}</h3>
+            <div id="icons">
+                {icons.map((data)=> <img id="icon" src={data.icon} alt={data.alt} />
+                )}
+            </div>
+            <p id="projDesc">{projDescription}</p>
+            <a id="gitLink" href={link} target='_blank'>
+                <img src={githubIcon} id="githubIcon" alt="github-icon" />
+                <p style={{margin:"auto"}}>visit</p>
+            </a>
+        </div>
     );
+    // return(
+    //     <a href={link} target='_blank'>
+    //         <img src={src} className="hover" alt={`${h3} logo`} />
+    //         <h3>{h3}</h3>
+    //         <p>{p}</p>
+    //     </a>
+    // );
 }
 
 export default ProjectCard;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,29 +1,61 @@
 import styles from './ProjectsStyles.module.css';
-import viber from '../../assets/viberr.png'
+// import viber from '../../assets/viberr.png'
 import ProjectCard from '../../common/ProjectCard';
+import jsLogo from '../../assets/javascript-svgrepo-com.svg';
+import reactLogo from '../../assets/react-svgrepo-com.svg';
+
 function Projects(){
+    const projOneIcons=[
+        {
+            icon:jsLogo,
+            alt:"JavaScript",
+        },
+        {
+            icon: reactLogo,
+            alt:"React",
+        }
+    ];
     return(
         <section id='projects' className={styles.container}>
             <h1 className='sectionTitle'>Projects</h1>
             <div className={styles.projectContainer}>
-            <ProjectCard
-                src={viber}
-                link="github-link"
-                h3="viberr"
-                p="Streaming App"
-            />
-            <ProjectCard
-                src={viber}
-                link="github-link"
-                h3="viberr"
-                p="Streaming App"
-            />
-            <ProjectCard
-                src={viber}
-                link="github-link"
-                h3="viberr"
-                p="Streaming App"
-            />
+            
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
+                <ProjectCard
+                    projName="Project Title"
+                    projDescription="this is a project description"
+                    link="github.com"
+                    icons={projOneIcons}
+                />
             </div>
         </section>
     )
